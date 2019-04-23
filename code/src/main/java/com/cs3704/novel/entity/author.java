589227a51bbbfda1d1c8ee4c229e081f438a1430
Mingchi Li <mingchili@Mingchis-MacBook-Pro.local>
2019-04-23 13:29:00 -0400

    update diagrams

--- code/src/main/java/com/cs3704/novel/entity/author.java
+++ code/src/main/java/com/cs3704/novel/entity/author.java
@@ -1,4 +1,32 @@
 package com.cs3704.novel.entity;
 
+import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "author")
+@Setter @Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class author {
+    @SequenceGenerator(name = "author_id", sequenceName = "author_id", allocationSize = 1)
+    @GeneratedValue(generator = "author_id")
+
+    @Id
+    private Long id;
+
+    @Column @UniqueElements
+    private String userName;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column
+    private String contact;
 }
--- code/src/main/java/com/cs3704/novel/entity/novel.java
+++ code/src/main/java/com/cs3704/novel/entity/novel.java
@@ -1,4 +1,25 @@
 package com.cs3704.novel.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "novel")
+@Setter @Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class novel {
+
+    @SequenceGenerator(name = "novel_id", sequenceName = "novel_id", allocationSize = 1)
+    @GeneratedValue(generator = "novel_id")
+
+    @Id
+    private Long id;
+
+    @Column
+    private String title;
+
+
 }
