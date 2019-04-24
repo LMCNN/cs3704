786595ea1c418ff2e3cc1b89a7573fc240202c68
Mingchi Li <mingchili@Mingchis-MacBook-Pro.local>
2019-04-24 16:18:37 -0400

    delete sql files

--- sql/load.sql
+++
@@ -1,9 +0,0 @@
--- psql -U postgres -d novels -a -f load.sql
-
-insert into author (contact, name, password, user_name) values ('admin@abc.com', 'admin', '123456', 'admin');
-
-insert into novel (title) values ('Test1');
-insert into novel (title) values ('Test2');
-
-insert into author_novel (author_id, novel_id) values (1, 1);
-insert into author_novel (author_id, novel_id) values (1, 2);
